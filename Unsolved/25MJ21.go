3fec4c1711fd2411dd24088a001693871795e214
90xv <g2201173@gmail.com>
2026-05-21 18:47:24 +0800

    solved 25ON23. created 25MJ21

---
+++ Unsolved/25MJ21.go
@@ -0,0 +1,22 @@
+// May Jun 2025 Paper Code 21
+// 1D Array CompetitorName [] has 25 names of competitors
+
+// 2D Array has all 25 CompetitorScores[][] in 5 events.
+// The first dimension of this array contains the index from CompetitorName []
+// The second dimension has their scores.
+// The competitors have a score between 1-100 and are manually entered into CompetitorScore[].
+
+// Medals are given to
+// 1) competitors who have the the highest points scored in each of the five events
+// 2) Competitors with the most medals
+
+// The 1D array Points[] stores the total number of points for each competitor.
+
+// s
+package mj2521
+
+import "fmt"
+
+func MY2521() {
+	fmt.Println("Hi There!")
+}
